cde54a90f282539e5da3688d6b2c37f9df62d5b1
Willams <willams-barbosa@outlook.com>
2025-03-12 15:52:29 -0300

    Correção do nome do usuário do teste da feature Transferencia de Dinheiro

--- cypress/e2e/CypressStudio/TransferenciaDeDinheiro.cy.js
+++ cypress/e2e/CypressStudio/TransferenciaDeDinheiro.cy.js
@@ -2,15 +2,15 @@ describe('Validar a transferência de dinheiro', () => {
     it('Ter duas contas e validar a transferência de dinheiro entre elas', () => {
       cy.visit('https://parabank.parasoft.com/parabank/index.htm')
       /* ==== Generated with Cypress Studio ==== */
-      cy.get(':nth-child(2) > .input').clear('j');
-      cy.get(':nth-child(2) > .input').type('jhon');
+      cy.get(':nth-child(2) > .input').clear();
+      cy.get(':nth-child(2) > .input').type('john');
       cy.get(':nth-child(4) > .input').clear();
       cy.get(':nth-child(4) > .input').type('demo');
       cy.get(':nth-child(5) > .button').click();
       cy.get('#leftPanel > ul > :nth-child(3) > a').click();
-      cy.get('#amount').clear('9');
+      cy.get('#amount').clear();
       cy.get('#amount').type('999');
-      cy.get('#toAccountId').select('13788');
+      cy.get('#toAccountId').select('12456');
       cy.get(':nth-child(4) > .button').click();
       cy.get('#showResult > .title').click();
       cy.get('#rightPanel').click();
