10f12d9e7abec411c77aabee87bd051edc7cd8ca
PatyRosa <paty_rosa1@yahoo.com.br>
2025-03-13 09:50:39 -0300

    Adicionando a funcionalidade de realizar login através do cypress studio

---
+++ cypress/e2e/CypressStudio/RealizarLogin.spec.cy.js
@@ -0,0 +1,12 @@
+describe('template spec', () => {
+    it('passes', () => {
+      cy.visit('https://parabank.parasoft.com/parabank/index.htm')
+      /* ==== Generated with Cypress Studio ==== */
+      cy.get(':nth-child(2) > .input').type('patyrosa');
+      cy.get(':nth-child(4) > .input').type('123456');
+      cy.get(':nth-child(5) > .button').click();
+      cy.get('.smallText').click();
+      cy.get('#showOverview > .title').click();
+      /* ==== End Cypress Studio ==== */
+    })
+  })
