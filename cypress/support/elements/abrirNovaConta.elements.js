ccef694aacb6ef7a48b46ba03319c2c217c0d524
Beatriz de Brito <bedebrito.web@gmail.com>
2025-03-18 12:43:30 -0300

    implementação pageobject na funcionalidade de abertura de conta

---
+++ cypress/support/elements/abrirNovaConta.elements.js
@@ -0,0 +1,13 @@
+export const abrirNovaContaElements = {
+    openNewAccountMenuLink: '#leftPanel > ul > :nth-child(1) > a',
+    typeOfAccountSelect: '#type',
+    accountIdSelect: '#fromAccountId',
+    openNewAccountButton: '#openAccountForm input[type=button]'
+}
+
+export const validarAbrirNovaContaElements = {
+    openAccountTitle: '#openAccountResult > .title',
+    openAccountText: '#openAccountResult > p:nth-child(2)',
+    newAccountText: '#openAccountResult > p:nth-child(3) > b',
+    newAccountId: '#newAccountId'
+}
