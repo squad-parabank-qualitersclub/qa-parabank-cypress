4571e45aeb4c332db48d67229bf234604dc7b93d
Priscila Caimi <priscila.caimi@hotmail.com>
2025-03-27 20:52:16 -0300

    adicionando cy.fixture ao codigo

--- cypress/e2e/PageObject/RealizarLogin.spec.cy.js
+++ cypress/e2e/PageObject/RealizarLogin.spec.cy.js
@@ -1,10 +1,14 @@
 import LoginPageObject from '../../support/pageObject/LoginComSucesso.pageObject'
 
 describe('Feature: Logar com sucesso', () => {
+  let userData;
  before (() => {
     cy.visit('/');
+    cy.fixture('environment.json').then((data)=>{
+      userData = data
+    })
  });
  it('Validar o login com sucesso', () => {
-   LoginPageObject.preencherFormularioLogin();
+   LoginPageObject.preencherFormularioLogin(userData.login);
  });
 });
--- cypress/support/pageObject/LoginComSucesso.pageObject.js
+++ cypress/support/pageObject/LoginComSucesso.pageObject.js
@@ -1,9 +1,9 @@
 import { realizarLoginElements } from '../elements/realizarLogin.elements';
 
 class LoginPageObject {
-  preencherFormularioLogin() {
-    cy.get(realizarLoginElements.inputFirstName).type('patyrosa');  // Nome de usuário
-    cy.get(realizarLoginElements.inputPassword).type('123456');  // Senha
+  preencherFormularioLogin(userData) {
+    cy.get(realizarLoginElements.inputFirstName).type(userData.userName);  // Nome de usuário
+    cy.get(realizarLoginElements.inputPassword).type(userData.senha);  // Senha
     cy.get(realizarLoginElements.inputSubmit).click();  // Clicar no botão de login
   }
   
